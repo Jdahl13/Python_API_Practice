262b5cbecac136b95f57723b821e7f531e92e9ed
jdahl13 <jdahl07@gmail.com>
2024-11-15 18:02:35 -0800

    Testing a good api to see 200 response

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 
-response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+#response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+#print(response.status_code) should return 404 error code
+
+response = requests.get("http://api.open-notify.org/astros")
 print(response.status_code)
+#This should return 200 if successful
